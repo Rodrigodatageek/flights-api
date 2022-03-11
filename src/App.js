81a149e6ad630b1a797c28f99a4ce4878f46363b
Rodrigo Fernandes <Rodrigodatageek@gmail.com>
2022-03-11 10:28:17 +0100

    1st push with new structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const url = 'https://api.skypicker.com/flights?fly_from=PRG&fly_to=VLC&partner=data4youcbp202106&limit=5';
+
+  const [flight, setFlight] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const resp = await fetch(url);
+    const result = await resp.json();
+    console.log(result);
+
+  }
   return (
     <div className="App">
-      <h1>Hello</h1>
+     {/* <Router>
+        <Header />
+
+        <Routes>
+           <Route></Route>
+           <Route></Route>
+           <Route></Route>
+        </Routes>
+
+      </Router> */}
+
       
+   
     </div>
   );
-}
+};
+
 
 export default App;

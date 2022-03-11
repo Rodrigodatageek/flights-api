c26ce66e34642b87f789b1a3e85a17a2eaac6033
Rodrigo Fernandes <Rodrigodatageek@gmail.com>
2022-03-11 14:35:18 +0100

    issues

--- src/App.js
+++ src/App.js
@@ -3,11 +3,8 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
-<<<<<<< HEAD
-=======
 import SearchResults from './components/SearchResults';
 import SearchContainer from './components/SearchContainer';
->>>>>>> ffd42494fe4429f4686534981f059d05d3a03a38
 
 function App() {
 

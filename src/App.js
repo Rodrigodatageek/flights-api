09b927db71209ae8fa7194142078faeea51dc982
Gheorghi Ivanciuc <grishan4ik@hotmail.com>
2022-03-11 13:01:07 +0100

    'searchresults'

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
+import SearchResults from './components/SearchResults';
 
 function App() {
 
   const url = 'https://api.skypicker.com/flights?fly_from=PRG&fly_to=VLC&partner=data4youcbp202106&limit=5';
 
   const [flight, setFlight] = useState([]);
+  const [data, setData] = useState([]);
   useEffect(() => {
     fetchData();
   }, []);
@@ -16,21 +18,23 @@ function App() {
   const fetchData = async () => {
     const resp = await fetch(url);
     const result = await resp.json();
-    console.log(result);
-
+    console.log(result.data);
+    setData(result.data);
   }
   return (
     <div className="App">
-     {/* <Router>
-        <Header />
-
+     <Router>
+        {/* <Header /> */}
+        
         <Routes>
-           <Route></Route>
-           <Route></Route>
-           <Route></Route>
+        {/* <SearchResults data={data}/> */}
+        <Route path="/result" element={<SearchResults data={data}/>} />
         </Routes>
 
-      </Router> */}
+           {/* <Route></Route>
+           <Route></Route> */}
+
+      </Router>
 
       
    
